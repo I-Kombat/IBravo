7c46740916c77213d78bb109234b30d7efb03bc8
Sajidh Zahir <sajidhzazahir@gmail.com>
2017-10-27 10:04:51 +0530

    Update IBravo.java

--- IBravo.java
+++ IBravo.java
@@ -3,6 +3,7 @@ import robocode.ScannedRobotEvent;
 import robocode.WinEvent;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 import robocode.BravoBot;
+import robocode.HitRobotEvent;
 
 import java.awt.*;
 
@@ -10,50 +11,57 @@ import java.awt.*;
 
 public class IBravo extends BravoBot {
 
-
+	int turnDirection = 1; // Clockwise or counterclockwise
 	public void run() {
 		// Set colors
-		setBodyColor(Color.pink);
-		setGunColor(Color.pink);
-		setRadarColor(Color.pink);
-		setScanColor(Color.pink);
-		setBulletColor(Color.pink);
-
-		// Loop forever
+		setBodyColor(Color.black);
+		setGunColor(Color.black);
+		setRadarColor(Color.black);
 		while (true) {
-			turnGunRight(10); // Scans automatically
+			turnRight(5 * turnDirection);
 		}
 	}
 
-	public void onRobotScanned(ScannedRobotEvent e) {
-		// Calculate exact location of the robot
-		double absoluteBearing = getHeading() + e.getBearing();
-		double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
+	/**
+	 * onScannedRobot:  We have a target.  Go get it.
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
 
-		// If it's close enough, fire!
-		if (Math.abs(bearingFromGun) <= 3) {
-			turnGunRight(bearingFromGun);
-			// We check gun heat here, because calling fire()
-			// uses a turn, which could cause us to lose track
-			// of the other robot.
-			if (getGunHeat() == 0) {
-				fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
-			}
-		} // otherwise just set the gun to turn.
-		// Note:  This will have no effect until we call scan()
-		else {
-			turnGunRight(bearingFromGun);
+		if (e.getBearing() >= 0) {
+			turnDirection = 1;
+		} else {
+			turnDirection = -1;
 		}
-		// Generates another scan event if we see a robot.
-		// We only need to call this if the gun (and therefore radar)
-		// are not turning.  Otherwise, scan is called automatically.
-		if (bearingFromGun == 0) {
-			scan();
-		}
+
+		turnRight(e.getBearing());
+		ahead(e.getDistance() + 5);
+		scan(); // Might want to move ahead again!
 	}
 
-	public void onWin(WinEvent e) {
-		// Victory dance
-		turnRight(36000);
-	}
+	/**
+	 * onHitRobot:  Turn to face robot, fire hard, and ram him again!
+	 */
+	public void onHitRobot(HitRobotEvent e) {
+		if (e.getBearing() >= 0) {
+			turnDirection = 1;
+		} else {
+			turnDirection = -1;
+		}
+		turnRight(e.getBearing());
+
+		// Determine a shot that won't kill the robot...
+		// We want to ram him instead for bonus points
+		if (e.getEnergy() > 16) {
+			fire(3);
+		} else if (e.getEnergy() > 10) {
+			fire(2);
+		} else if (e.getEnergy() > 4) {
+			fire(1);
+		} else if (e.getEnergy() > 2) {
+			fire(.5);
+		} else if (e.getEnergy() > .4) {
+			fire(.1);
+		}
+		ahead(40); // Ram him again!
+}
 } 
