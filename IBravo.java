7977e14f7dbc9caaab5163e09ba9b931833ae1d7
Sajidh Zahir <sajidhzazahir@gmail.com>
2017-10-27 10:50:45 +0530

    Update IBravo.java

--- IBravo.java
+++ IBravo.java
@@ -2,24 +2,27 @@ import robocode.Robot;
 import robocode.ScannedRobotEvent;
 import robocode.WinEvent;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
+
+import robocode.AdvancedRobot;
 import robocode.BravoBot;
 import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
 
 import java.awt.*;
 
 
 
 public class IBravo extends BravoBot {
-
 	int turnDirection = 1; // Clockwise or counterclockwise
+
+	SuperTracker tr;
+	
+    public IBravo() {
+    	tr = new SuperTracker();
+    }
+	
 	public void run() {
-		// Set colors
-		setBodyColor(Color.black);
-		setGunColor(Color.black);
-		setRadarColor(Color.black);
-		while (true) {
-			turnRight(5 * turnDirection);
-		}
+		tr.run();
 	}
 
 	/**
@@ -27,41 +30,72 @@ public class IBravo extends BravoBot {
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
 
-		if (e.getBearing() >= 0) {
-			turnDirection = 1;
-		} else {
-			turnDirection = -1;
-		}
-
-		turnRight(e.getBearing());
-		ahead(e.getDistance() + 5);
-		scan(); // Might want to move ahead again!
+		tr.onScannedRobot(e);
 	}
 
 	/**
 	 * onHitRobot:  Turn to face robot, fire hard, and ram him again!
 	 */
-	public void onHitRobot(HitRobotEvent e) {
-		if (e.getBearing() >= 0) {
-			turnDirection = 1;
-		} else {
-			turnDirection = -1;
-		}
-		turnRight(e.getBearing());
-
-		// Determine a shot that won't kill the robot...
-		// We want to ram him instead for bonus points
-		if (e.getEnergy() > 16) {
-			fire(3);
-		} else if (e.getEnergy() > 10) {
-			fire(2);
-		} else if (e.getEnergy() > 4) {
-			fire(1);
-		} else if (e.getEnergy() > 2) {
-			fire(.5);
-		} else if (e.getEnergy() > .4) {
-			fire(.1);
-		}
-		ahead(40); // Ram him again!
-}
+	public void onHitWall(HitWallEvent e){
+		tr.onHitWall(e);
+	}
+	 public void onWin(WinEvent e) {
+	        tr.onWin(e);
+	 }
 } 
+
+class SuperTracker extends AdvancedRobot {
+    int moveDirection=1;//which way to move
+    /**
+     * run:  Tracker's main run function
+     */
+    public void run() {
+        setAdjustRadarForRobotTurn(true);//keep the radar still while we turn
+        setBodyColor(new Color(128, 128, 50));
+        setGunColor(new Color(50, 50, 20));
+        setRadarColor(new Color(200, 200, 70));
+        setScanColor(Color.white);
+        setBulletColor(Color.blue);
+        setAdjustGunForRobotTurn(true); // Keep the gun still when we turn
+        turnRadarRightRadians(Double.POSITIVE_INFINITY);//keep turning radar right
+    }
+
+    /**
+     * onScannedRobot:  Here's the good stuff
+     */
+    public void onScannedRobot(ScannedRobotEvent e) {
+        double absBearing=e.getBearingRadians()+getHeadingRadians();//enemies absolute bearing
+        double latVel=e.getVelocity() * Math.sin(e.getHeadingRadians() -absBearing);//enemies later velocity
+        double gunTurnAmt;//amount to turn our gun
+        setTurnRadarLeftRadians(getRadarTurnRemainingRadians());//lock on the radar
+        if(Math.random()>.9){
+            setMaxVelocity((12*Math.random())+12);//randomly change speed
+        }
+        if (e.getDistance() > 150) {//if distance is greater than 150
+            gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing- getGunHeadingRadians()+latVel/22);//amount to turn our gun, lead just a little bit
+            setTurnGunRightRadians(gunTurnAmt); //turn our gun
+            setTurnRightRadians(robocode.util.Utils.normalRelativeAngle(absBearing-getHeadingRadians()+latVel/getVelocity()));//drive towards the enemies predicted future location
+            setAhead((e.getDistance() - 140)*moveDirection);//move forward
+            setFire(3);//fire
+        }
+        else{//if we are close enough...
+            gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing- getGunHeadingRadians()+latVel/15);//amount to turn our gun, lead just a little bit
+            setTurnGunRightRadians(gunTurnAmt);//turn our gun
+            setTurnLeft(-90-e.getBearing()); //turn perpendicular to the enemy
+            setAhead((e.getDistance() - 140)*moveDirection);//move forward
+            setFire(3);//fire
+        }
+    }
+    public void onHitWall(HitWallEvent e){
+        moveDirection=-moveDirection;//reverse direction upon hitting a wall
+    }
+    /**
+     * onWin:  Do a victory dance
+     */
+    public void onWin(WinEvent e) {
+        for (int i = 0; i < 50; i++) {
+            turnRight(30);
+            turnLeft(30);
+        }
+    }
+}
